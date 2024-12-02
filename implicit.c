31da0153eb9b0be7a62c79b39d065b959adcda24
CS107 tools <cs107@cs.stanford.edu>
2024-12-02 13:50:23 -0800

    successful make.

--- implicit.c
+++ implicit.c
@@ -73,10 +73,10 @@ void *mymalloc(size_t requested_size) {
         if (isfree(current_header) && getsize(current_header) >= needed) { // if header indicates free block and there is enough size
             // check if we can create another header after we allocate memory
             if (getsize(current_header) - needed >= sizeof(header) + ALIGNMENT) {  // header needs the space for the size of itself and at least 8 bytes
-                size_t remaining = getsize(current_header);
+                size_t surplus = getsize(current_header);
                 current_header->data = needed;
                 header *next = (header*)((char*)current_header + sizeof(header) + needed); // create new header
-                next->data = remaining - needed - sizeof(header);
+                next->data = surplus - needed - sizeof(header);
             }
             
             current_header->data += 1; // change header from indicating free to allocated
