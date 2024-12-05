c6bb4a04ff955361fd9771f5bed05a8b60d7042c
CS107 tools <cs107@cs.stanford.edu>
2024-12-05 03:04:55 -0800

    successful make.

--- explicit.c
+++ explicit.c
@@ -169,7 +169,7 @@ void *myrealloc(void *old_ptr, size_t new_size) {
     myfree(old_ptr);
     void *new_ptr = NULL;
     new_ptr = mymalloc(new_size);
-    memcpy(new_ptr, old_ptr, new_size);
+    memcpy(new_ptr, old_ptr, cur_size);
     return new_ptr;
 
 }
