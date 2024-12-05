5065d6bc7e6a7cb92349de3e43ca27cd71bb13f1
CS107 tools <cs107@cs.stanford.edu>
2024-12-05 03:16:56 -0800

    successful make.

--- explicit.c
+++ explicit.c
@@ -166,10 +166,10 @@ void *myrealloc(void *old_ptr, size_t new_size) {
         }
     }
     // in-place does not work
-    myfree(old_ptr);
     void *new_ptr = NULL;
     new_ptr = mymalloc(new_size);
     memcpy(new_ptr, old_ptr, cur_size);
+    free(old_ptr);
     return new_ptr;
 
 }
