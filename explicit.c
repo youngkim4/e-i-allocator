c868f54bb32a86f30d615ca24aaa2339d2c29b6a
CS107 tools <cs107@cs.stanford.edu>
2024-12-04 18:33:02 -0800

    successful make.

--- explicit.c
+++ explicit.c
@@ -156,6 +156,10 @@ void *myrealloc(void *old_ptr, size_t new_size) {
     if (cur_size >= new_size) {
         if(cur_size - new_size >= sizeof(header) + 2*ALIGNMENT) {
             split(nf, new_size);
+            (nf->h).data += 1;
+        }
+        else {
+            (nf->h).data = new_size + 1;
         }
         
         return old_ptr;
